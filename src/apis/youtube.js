c7dc1fb33655c48e315e70f6811e1cde98e295be
Philip Oka <philip.oka@gmail.com>
2021-03-29 13:07:11 -0300

    protecting api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyA4Y2e1F_ddTooBt67jE717crmiAAPAbww';
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
